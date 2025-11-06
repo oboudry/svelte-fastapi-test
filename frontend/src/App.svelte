3105bc885b76c93736ffcc435d5039bec38608f9
Olivier Boudry <olivier.boudry@gmail.com>
2025-11-06 21:35:43 +0100

    Initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let message = "Loading..."
+  fetch("http://127.0.0.1:8000/api/hello")
+    .then(r => r.json())
+    .then(data => message = data.message)
+</script>
+
+<main>
+  <h1>{message}</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding-top: 4rem;
+    font-family: sans-serif;
+  }
+</style>
+
